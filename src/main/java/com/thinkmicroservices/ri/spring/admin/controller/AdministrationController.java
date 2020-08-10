172d966d88105a7cf4dad32382536e6fe10c0f84
cwoodward <cwoodward@thinkmicroservices>
2020-08-09 21:41:57 -0400

    added method javadocs

--- src/main/java/com/thinkmicroservices/ri/spring/admin/controller/AdministrationController.java
+++ src/main/java/com/thinkmicroservices/ri/spring/admin/controller/AdministrationController.java
@@ -1,7 +1,5 @@
- 
 package com.thinkmicroservices.ri.spring.admin.controller;
 
-import com.thinkmicroservices.ri.spring.admin.jwt.JWT;
 import com.thinkmicroservices.ri.spring.admin.jwt.JWTService;
 import com.thinkmicroservices.ri.spring.admin.model.Profile;
 import com.thinkmicroservices.ri.spring.admin.service.DiscoveryService;
@@ -12,18 +10,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiParam;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import static org.reflections.Reflections.collect;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,11 +31,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(value = "Administration Management System", description = "Operations pertaining to application-level administraion")
 @Slf4j
-//@CrossOrigin(origins = {"${controller.cors.origin}"}, allowedHeaders = "*")
+ 
 
 @RestController
 public class AdministrationController {
 
+    
     @Autowired
     private UserAdminService userAdminService;
 
@@ -53,6 +46,15 @@ public class AdministrationController {
     @Autowired
     private JWTService jwtService;
 
+    /**
+     * 
+     * @param pageNo
+     * @param pageSize
+     * @param sortBy
+     * @param like
+     * @param httpServletRequest
+     * @return 
+     */
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     @ApiImplicitParams({
         @ApiImplicitParam(name = "Authorization", value = "Authorization token",
@@ -63,18 +65,19 @@ public class AdministrationController {
             @ApiParam("<Optional> Regex to filter (applied to first,middle and last name fields)- default to all /./") @RequestParam(defaultValue = ".") String like, HttpServletRequest httpServletRequest) {
         log.debug("get user profiles");
 
-       // JWT jwt = jwtService.getJWT(httpServletRequest);
-       // if (jwt == null) {
-       //     return new ResponseEntity<RestResponsePage<Profile>>(HttpStatus.UNAUTHORIZED);
-       // }
-      //  if (jwt.hasRole(JWT.ROLE_ADMIN)) {
-            RestResponsePage<Profile> userProfiles = userAdminService.findUserProfilesByPage(pageNo, pageSize, sortBy, like, httpServletRequest.getHeader("Authorization"));
+        RestResponsePage<Profile> userProfiles = userAdminService.findUserProfilesByPage(pageNo, pageSize, sortBy, like, httpServletRequest.getHeader("Authorization"));
 
-            return new ResponseEntity<>(userProfiles, HttpStatus.OK);
-      //  }
-       // return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(userProfiles, HttpStatus.OK);
+
     }
 
+    /**
+     * 
+     * @param accountId
+     * @param activeStatus
+     * @param httpServletRequest
+     * @return 
+     */
     @RequestMapping(value = "/setUserActiveStatus/{accountId}", method = RequestMethod.POST)
     @ApiImplicitParams({
         @ApiImplicitParam(name = "Authorization", value = "Authorization token",
@@ -85,16 +88,9 @@ public class AdministrationController {
 
         String authHeader = httpServletRequest.getHeader("Authorization");
         log.debug("get user profiles");
-       // JWT jwt = jwtService.getJWT(httpServletRequest);
 
-      //  if (jwt == null) {
-         //   return new ResponseEntity<RestResponsePage<Profile>>(HttpStatus.UNAUTHORIZED);
-       // }
+        return ResponseEntity.ok(this.userAdminService.setUserActiveStatus(accountId, activeStatus, authHeader));
 
-      //  if (jwt.hasRole(JWT.ROLE_ADMIN)) {
-            return ResponseEntity.ok(this.userAdminService.setUserActiveStatus(accountId, activeStatus, authHeader));
-       // }
-       // return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
     @RequestMapping(value = "/getServiceInstances", method = RequestMethod.GET)
@@ -105,26 +101,22 @@ public class AdministrationController {
         return ResponseEntity.ok(this.discoveryService.getAllServiceInstances());
     }
 
+    /**
+     * 
+     * @param request
+     * @param httpServletRequest
+     * @return 
+     */
     @PostMapping(path = "/getTelemetryByAccountId")
     @ResponseBody
     @ApiImplicitParams({
         @ApiImplicitParam(name = "Authorization", value = "Authorization token",
                 required = true, dataType = "string", paramType = "header")})
-    public ResponseEntity<PagedTelemetryEventResponse<ClientTelemetryEvent>  > findByAccountId(@RequestBody PagedTelemetryEventRequest request, HttpServletRequest httpServletRequest) {
+    public ResponseEntity<PagedTelemetryEventResponse<ClientTelemetryEvent>> findByAccountId(@RequestBody PagedTelemetryEventRequest request, HttpServletRequest httpServletRequest) {
 
-        //JWT jwt = jwtService.getJWT(httpServletRequest);
-        //if (jwt == null) {
-        //    return new ResponseEntity<RestResponsePage<Profile>>(HttpStatus.UNAUTHORIZED);
-       // }
-       // if (jwt.hasRole(JWT.ROLE_ADMIN)) {
-            PagedTelemetryEventResponse<ClientTelemetryEvent> pagedResult = userAdminService.findTelemetryByPage(request, httpServletRequest.getHeader("Authorization"));
-            
-           
-        
-         
-        
-        return ResponseEntity.ok( pagedResult);
-       // }
-       // return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        PagedTelemetryEventResponse<ClientTelemetryEvent> pagedResult = userAdminService.findTelemetryByPage(request, httpServletRequest.getHeader("Authorization"));
+
+        return ResponseEntity.ok(pagedResult);
+
     }
 }
